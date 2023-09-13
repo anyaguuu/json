e3c217904aa01279ffa227da16432fbdd64ebed4
anyaguuu <ag156@rice.edu>
2023-09-13 14:55:19 -0500

    validate data

--- schema/main.go
+++ schema/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"log/slog"
 	"os"
 
 	"github.com/santhosh-tekuri/jsonschema/v5"
@@ -18,7 +19,8 @@ func main() {
 	var jsonData []byte
 	var jsonFile *os.File
 	var err error
-	var data any
+	var data []byte
+	var d interface{}
 
 	// Take in cli arg for file containing JSON data
 	flag.StringVar(&jsonDataFilename, "j", "", "File containing JSON data")
@@ -29,12 +31,18 @@ func main() {
 	compiler = jsonschema.NewCompiler()
 	// Compile the JSON schema
 	schema, err = compiler.Compile(jsonSchemaFilename)
+	if err != nil {
+		slog.Error("schema compilation error", "error", err)
+		return
+	}
 
-	// Read file contents
+	// Open JSON file
 	jsonFile, err = os.Open(jsonDataFilename) // For read access.
 	if err != nil {
 		fmt.Println("error with opening file")
 	}
+
+	// Read contents of JSON file
 	jsonData, err = os.ReadFile(jsonDataFilename)
 	if err != nil {
 		log.Fatal(err)
@@ -42,7 +50,17 @@ func main() {
 	fmt.Printf("read %q", jsonData)
 
 	// Unmarshal json data, apply visitor
-	data = json.Unmarshal(jsonData, &data)
+	if err := json.Unmarshal(data, &d); err != nil {
+		slog.Error("unable to unmarshal data", "data", data, "error", err)
+		return
+	}
+
+	// Validate json against schema
+	if err := schema.Validate(d); err != nil {
+		msg := fmt.Sprintf("%#v", err)
+		slog.Error("data does not conform to the schema", "error", msg)
+	}
+	fmt.Println("JSON data conforms to the schema")
 
 	// Call Accept(data, visitor) with visitor object
 

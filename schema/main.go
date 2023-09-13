a7370d3eb0318db966d998d9812e9e5477b2896c
anyaguuu <ag156@rice.edu>
2023-09-13 14:11:08 -0500

    rename

--- schema/main.go
+++ schema/main.go
@@ -7,22 +7,22 @@ import (
 )
 
 func main() {
-	var filename string
+	var jsonDataFilename string
 	var contents []byte
 	var reader *Reader
 	var file *os.File
 	var err error
 
 	// Take in cli arg for file containing JSON data
-	flag.StringVar(&filename, "f", "", "File containing JSON data")
+	flag.StringVar(&jsonDataFilename, "f", "", "File containing JSON data")
 	flag.Parse()
 
 	// Read file and unmarshal contents into var type any
-	file, err = os.Open(filename) // For read access.
+	file, err = os.Open(jsonDataFilename) // For read access.
 	if err != nil {
 		fmt.Println("error with opening file")
 	}
-	contents = 
+	// contents =
 
 	if err = file.Close(); err != nil {
 		fmt.Println("error with closing file")
